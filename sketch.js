a4b594af6406635ef1f9e2e0937f1540bb9ed32b
LuizOt1a <00001107962031SP@al.educacao.sp.gov.br>
2024-08-15 15:41:52 -0300

    adicionando css e js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let cor;
+let posicaoHorizontal //
+let posicaoVertical //
+
+function setup() {
+  createCanvas(400, 400);
+  background(400, 400)
+  cor
+  
+
+function draw() {
+  background(220);
+}
